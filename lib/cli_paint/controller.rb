c216249f1ed204651f6422b89f391c8594d99501
Anatoliy Kukul <anatolkukula@gmail.com>
2016-11-26 19:05:42 +0100

    Add line command to dispatch

--- lib/cli_paint/controller.rb
+++ lib/cli_paint/controller.rb
@@ -1,7 +1,8 @@
 module CliPaint
   class Controller
     GENERAL_ERR_MSG = "I don't know that command"
-    CREATE_ERR_MSG = "Wrong args for create command"
+    NO_CANVAS_ERR_MSG = "You need to create canvas first"
+    ARGS_ERR_MSG = "Wrong args for command"
 
     def initialize
       @canvas = nil
@@ -12,6 +13,8 @@ module CliPaint
       case command[0]
       when 'C'
         create_canvas(command)
+      when 'L'
+        draw_line(command)
       when 'Q'
         exit
       else
@@ -21,19 +24,31 @@ module CliPaint
 
     private
 
-    def validate_create_canvas(command)
+    def validate_args(command, number)
       return false unless
-        command.size == 3 &&
-        command[1].to_i > 0 &&
-        command[2].to_i > 0
+        command.size == number + 1 &&
+          command[1..number + 1].all? { |arg| arg.to_i > 0 }
 
       true
     end
 
-    def create_canvas(command)
-      return CREATE_ERR_MSG unless validate_create_canvas(command)
+    def validate_canvas_exists
+      return false if @canvas.nil?
+      true
+    end
 
-      @canvas = Canvas.new(command[1].to_i, command[2].to_i)
+    def create_canvas(command)
+      return ARGS_ERR_MSG unless validate_args(command, 2)
+
+      @canvas = Canvas.new(*command[1..2].map(&:to_i))
+      @canvas.to_s
+    end
+
+    def draw_line(command)
+      return NO_CANVAS_ERR_MSG unless validate_canvas_exists
+      return ARGS_ERR_MSG unless validate_args(command, 4)
+
+      @canvas.line(*command[1..4].map(&:to_i))
       @canvas.to_s
     end
   end
--- spec/cli_paint/controller_spec.rb
+++ spec/cli_paint/controller_spec.rb
@@ -28,16 +28,60 @@ describe CliPaint::Controller do
       context 'invalid arguments' do
         context 'not enough args' do
           it 'returns error message' do
-            expect(controller.dispatch('C 1')).to eq(CliPaint::Controller::CREATE_ERR_MSG)
+            expect(controller.dispatch('C 1')).to eq(CliPaint::Controller::ARGS_ERR_MSG)
           end
         end
 
         context 'non integer args' do
           it 'returns error message' do
-            expect(controller.dispatch('C Z A')).to eq(CliPaint::Controller::CREATE_ERR_MSG)
+            expect(controller.dispatch('C Z A')).to eq(CliPaint::Controller::ARGS_ERR_MSG)
           end
         end
       end
+    end
+
+    describe 'L command' do
+      context 'canvas created' do
+        before do
+          controller.dispatch('C 20 4')
+        end
+
+        context 'valid arguments' do
+          let(:expected) do
+            "----------------------\n" \
+              "|x                   |\n" \
+              "|x                   |\n" \
+              "|x                   |\n" \
+              "|x                   |\n" \
+              "----------------------"
+          end
+
+          it 'returns canvas' do
+            expect(controller.dispatch('L 1 1 1 4')).to eq(expected)
+          end
+        end
+
+        context 'invalid arguments' do
+          context 'not enough args' do
+            it 'returns error message' do
+              expect(controller.dispatch('L 1')).to eq(CliPaint::Controller::ARGS_ERR_MSG)
+            end
+          end
+
+          context 'non integer args' do
+            it 'returns error message' do
+              expect(controller.dispatch('L Z A O P')).to eq(CliPaint::Controller::ARGS_ERR_MSG)
+            end
+          end
+        end
+      end
+
+      context 'no canvas yet' do
+        it 'returns canvas' do
+          expect(controller.dispatch('L 1 1 1 4')).to eq(CliPaint::Controller::NO_CANVAS_ERR_MSG)
+        end
+      end
+
     end
   end
 end
